0d36c83ae5159b1b4f2cbc0c18a602bd2642589a
omuga <omuga@alumnos.inf.utfsm.cl>
2020-06-30 23:01:31 -0400

    Se añade crossorigins

--- src/main/java/com/example/isw/controllers/TiendaController.java
+++ src/main/java/com/example/isw/controllers/TiendaController.java
@@ -13,10 +13,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @RestController
 @RequestMapping("/tiendas")
+@CrossOrigin(origins = "*")
 public class TiendaController {
     
     @Autowired
